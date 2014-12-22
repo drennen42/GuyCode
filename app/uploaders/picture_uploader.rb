18838471aa178a45ca65311817e29f28e7e74192
Alex Drennen <drennen42@gmail.com>
2014-12-22 16:23:22 -0600

    added video tag for article videos

--- app/uploaders/picture_uploader.rb
+++ app/uploaders/picture_uploader.rb
@@ -10,14 +10,15 @@ class PictureUploader < CarrierWave::Uploader::Base
   storage :file
   # storage :fog
 
-  def cache_dir
-    "#{Rails.root}/tmp/uploads"
-  end
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
   def store_dir
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+  end
+
+  def cache_dir
+    "#{Rails.root}/tmp/uploads"
   end
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
