11b349a0416ecc73a51c9b447f713a357c9ce883
Alex Drennen <drennen42@gmail.com>
2014-12-22 15:53:13 -0600

    adding MOV filetype to carrierwave

--- app/uploaders/picture_uploader.rb
+++ app/uploaders/picture_uploader.rb
@@ -43,7 +43,7 @@ class PictureUploader < CarrierWave::Uploader::Base
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
   def extension_white_list
-    %w(pdf pptx jpg png gif mp3 m4a)
+    %w(pdf pptx jpg png gif mp3 m4a mov MOV)
   end
 
   # Override the filename of the uploaded files:
