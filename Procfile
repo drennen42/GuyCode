92eeeaa9681eb3e3882fdf4ef26957295228679b
Alex Drennen <drennen42@gmail.com>
2014-12-24 17:17:40 -0600

    trying to get S3 working

--- Procfile
+++ Procfile
@@ -1,1 +1,4 @@
-web: bundle exec rails server -p $PORT
+web:    bundle exec rails server -p $PORT
+web:    bundle exec thin start -p $PORT
+worker: bundle exec rake resque:work QUEUE=*
+clock:  bundle exec rake resque:scheduler
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,12 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  # def authenticate
+  #   if APP_CONFIG['perform_authentication']
+  #     authenticate_or_request_with_http_basic do |S3_BUCKET, AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY|
+  #       S3_BUCKET == APP_CONFIG['S3_BUCKET'] && AWS_ACCESS_KEY_ID == APP_CONFIG['AWS_ACCESS_KEY_ID'] && AWS_SECRET_ACCESS_KEY == APP_CONFIG['AWS_SECRET_ACCESS_KEY']
+  #     end
+  #   end
+  # end
 end
