0d700ac1affcc4e0f3c17af37df8c4aec73dbca6
Alex Drennen <drennen42@gmail.com>
2014-12-26 12:25:12 -0600

    fixed stuff

--- app/uploaders/picture_uploader.rb
+++ app/uploaders/picture_uploader.rb
@@ -14,13 +14,14 @@ class PictureUploader < CarrierWave::Uploader::Base
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
+  def cache_dir
+    "#{Rails.root}/tmp/uploads"
+  end
+
   def store_dir
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
 
-  def cache_dir
-    "#{Rails.root}/tmp/uploads"
-  end
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
   # def default_url
@@ -44,9 +45,9 @@ class PictureUploader < CarrierWave::Uploader::Base
 
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
-  # def extension_white_list
-  #   %w(pdf pptx jpg png gif mp3 m4a mov MOV)
-  # end
+  def extension_white_list
+    %w(pdf pptx jpg png gif mp3 m4a mov MOV)
+  end
 
   # Override the filename of the uploaded files:
   # Avoid using model.id or version_name here, see uploader/store.rb for details.
