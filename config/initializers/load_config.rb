f13033d811838b911ddc4b323e2e728df004795a
Alex Drennen <drennen42@gmail.com>
2014-12-26 11:47:07 -0600

    removed all instances of AWS keys from publicly accessed files on github

--- config/initializers/load_config.rb
+++ config/initializers/load_config.rb
@@ -1,7 +1,3 @@
 require 'yaml'
 
-ENV['S3_BUCKET_NAME'] = "guycode-assets"
-ENV['AWS_ACCESS_KEY'] = "AKIAILPDC2MIASCCK22Q"
-ENV['AWS_SECRET_ACCESS'] = "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"
-
 APP_CONFIG = YAML.load_file("#{Rails.root}/config/config.yml")[Rails.env]
